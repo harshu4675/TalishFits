ccf9a44f989f14c61e89fd4f4e5ee46f09dc6477
Harsh Solanki <harshu6278@gmail.com>
2026-06-30 22:29:33 +0530

    fixing server issue

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -5,7 +5,7 @@ const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  timeout: 30000,
+  timeout: 90000,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
